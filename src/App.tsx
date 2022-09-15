5837684d92f37299ac85ac90ed71273156d4e7ed
Faith <faiththe1rst@gmail.com>
2022-09-15 13:07:39 +0100

    testing new path

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import "./components/styles/global.scss";
 import { GetStarted } from "./components/pages/get-started";
 import { Routes, Route } from "react-router-dom";
 import Messaging from "./components/pages/messaging";
-import NotFound from "./components/pages/notFound";
+import NotFound from "./components/pages/404";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<GetStarted />} />
-        <Route path="chat_room" element={<Messaging />} />
+        <Route path="/chat_room" element={<Messaging />} />
         <Route path="*" element={<NotFound/>} />
       </Routes>
     </>
--- src/components/pages/404.tsx
+++ src/components/pages/404.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            not found
-        </div>
+        <div> not found ooooo </div>
     );
 };
 
